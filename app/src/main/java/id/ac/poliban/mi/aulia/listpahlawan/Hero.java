3b17420000b18d73b84b2a667ef03d62918966a9
NurRizkiAulia <kiicbr88@gmail.com>
2020-01-05 16:06:57 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/aulia/listpahlawan/Hero.java
@@ -0,0 +1,44 @@
+package id.ac.poliban.mi.aulia.listpahlawan;
+
+import androidx.annotation.NonNull;
+
+public class Hero {
+    private String image;
+    private String heroName;
+    private String heroDetail;
+
+    public Hero(String image, String heroName, String heroDetail) {
+        this.image = image;
+        this.heroName = heroName;
+        this.heroDetail = heroDetail;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getHeroDetail() {
+        return heroDetail;
+    }
+
+    public void setHeroDetail(String heroDetail) {
+        this.heroDetail = heroDetail;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%30s\n\n%s", getHeroName(), getHeroDetail());
+    }
+}
